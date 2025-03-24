5c9ec78b34a2ca988ee0c0b7d0116090a7e62f05
M.A.Felegari <M.A.Felegari@hotmail.com>
2025-03-24 08:20:16 +0330

    feat: extend User relations to other entities

--- src/PortoFree.Domain/Entities/User.cs
+++ src/PortoFree.Domain/Entities/User.cs
@@ -2,8 +2,12 @@
 
 public class User
 {
-    public IEnumerable<WorkExample>? WorkExamples { get; set; }
+    public int Id { get; set; } //temp id because we want to use identityUser
     public int UsedSpace { get; set; }
     public int TotalSpace { get; set; }
     public bool IsBanned { get; set; }
+    public IEnumerable<WorkExample>? WorkExamples { get; set; }
+    public IEnumerable<UserSkill>? UserSkills { get; set; }
+    public IEnumerable<EmploymentHistory>? EmploymentsHistory { get; set; }
+    public IEnumerable<Comment>? Comments { get; set; }
 }
