31983e3c9d947711c2c2524ee636bb45baddf9ef
M.A.Felegari <M.A.Felegari@hotmail.com>
2025-05-03 17:23:29 +0330

    chore: clarify UpdateWorkExampleHandler log

--- src/PortoFree.Application/Features/WorkExamples/Commands/UpdateWorkExample/UpdateWorkExampleCommandHandler.cs
+++ src/PortoFree.Application/Features/WorkExamples/Commands/UpdateWorkExample/UpdateWorkExampleCommandHandler.cs
@@ -35,7 +35,7 @@ public class UpdateWorkExampleCommandHandler : IRequestHandler<UpdateWorkExample
         var user = _currentUserContext.GetCurrentUser() ??
                    throw new UnauthenticatedException();
         
-        _logger.LogInformation("try to add new work example with data: {@request} by {user}",request);
+        _logger.LogInformation("try to update work example with data: {@request} by {user}",request);
         
         var workExample = await _workExamplesRepository.GetAsync(request.Id) ??
                           throw new NotFoundException(typeof(WorkExample), request.Id.ToString());
