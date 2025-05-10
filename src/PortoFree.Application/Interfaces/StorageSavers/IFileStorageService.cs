374aaf53090a38b61ae473085345df788dcdc973
M.A.Felegari <M.A.Felegari@hotmail.com>
2025-05-10 17:43:41 +0330

    feat: add DeleteFile to StorageService

--- src/PortoFree.Application/Interfaces/StorageSavers/IFileStorageService.cs
+++ src/PortoFree.Application/Interfaces/StorageSavers/IFileStorageService.cs
@@ -5,4 +5,5 @@ public interface IFileStorageService
     public string GetWebNormalizedPath(string physicalPath);
     public string GetPhysicalPath(string webNormalizedPath);
     public Task<string> SaveFileAsync(Stream stream, string fileName);
+    public void DeleteFile(string webNormalizedPath);
 }
--- src/PortoFree.Infrastructure/StorageSavers/FileStorageService.cs
+++ src/PortoFree.Infrastructure/StorageSavers/FileStorageService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using PortoFree.Application.Interfaces.Logging;
 using PortoFree.Application.Interfaces.StorageSavers;
 
 namespace PortoFree.Infrastructure.StorageSavers;
@@ -6,8 +7,10 @@ namespace PortoFree.Infrastructure.StorageSavers;
 public class FileStorageService : IFileStorageService
 {
     private readonly string _webRootPath;
-    public FileStorageService(IWebHostEnvironment webHostEnvironment)
+    private readonly IAppLogger<FileStorageService> _logger;
+    public FileStorageService(IWebHostEnvironment webHostEnvironment, IAppLogger<FileStorageService> logger)
     {
+        _logger = logger;
         _webRootPath = webHostEnvironment.WebRootPath;
     }
 
@@ -47,6 +50,23 @@ public class FileStorageService : IFileStorageService
         stream.Seek(0, SeekOrigin.Begin);
         await stream.CopyToAsync(fileStream);
         
+        _logger.LogInformation("File successfully saved at path: {path}",saveFullPath);
+        
         return GetWebNormalizedPath(saveFullPath);
+    }
+
+    public void DeleteFile(string webNormalizedPath)
+    {
+        var physicalPath = GetPhysicalPath(webNormalizedPath);
+
+        if (!File.Exists(physicalPath))
+        {
+            _logger.LogWarning("try to delete file but File does not exist at {path}", physicalPath);
+            throw new FileNotFoundException($"File {physicalPath} not found.");
+        }
+        
+        File.Delete(physicalPath);
+        
+        _logger.LogInformation("File at path: {path} successfully deleted", physicalPath);
     }
 }
